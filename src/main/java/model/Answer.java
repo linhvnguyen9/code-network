88fb89c978599f8e51caae2150c1b2563e146de9
levunam30101999 <levunam30101999@gmail.com>
2020-12-17 21:06:48 +0700

    Khong lombok

--- src/main/java/model/Answer.java
+++ src/main/java/model/Answer.java
@@ -1,16 +1,7 @@
 package model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import java.io.Serializable;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class Answer implements Serializable {
     private static final long serialVersionUID = 2L;
     private Student student;
@@ -25,4 +16,140 @@ public class Answer implements Serializable {
     private boolean isNormalizationStringAnswerRight;
     private boolean isBSCNNNumericAnswerRight;
     private boolean isUSCLNNumericAnswerRight;
+
+    public Answer() {
+    }
+
+    public Answer(Student student, String reverseStringAnswer, String normalizationStringAnswer, int maxNumericAnswer, int uclnNumericAnswer, int bcnnNumericAnswer, boolean isAlreadyRegistration, boolean isReverseStringAnswerRight, boolean isMaxNumericAnswerRight, boolean isNormalizationStringAnswerRight, boolean isBSCNNNumericAnswerRight, boolean isUSCLNNumericAnswerRight) {
+        this.student = student;
+        this.reverseStringAnswer = reverseStringAnswer;
+        this.normalizationStringAnswer = normalizationStringAnswer;
+        this.maxNumericAnswer = maxNumericAnswer;
+        this.uclnNumericAnswer = uclnNumericAnswer;
+        this.bcnnNumericAnswer = bcnnNumericAnswer;
+        this.isAlreadyRegistration = isAlreadyRegistration;
+        this.isReverseStringAnswerRight = isReverseStringAnswerRight;
+        this.isMaxNumericAnswerRight = isMaxNumericAnswerRight;
+        this.isNormalizationStringAnswerRight = isNormalizationStringAnswerRight;
+        this.isBSCNNNumericAnswerRight = isBSCNNNumericAnswerRight;
+        this.isUSCLNNumericAnswerRight = isUSCLNNumericAnswerRight;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getReverseStringAnswer() {
+        return reverseStringAnswer;
+    }
+
+    public void setReverseStringAnswer(String reverseStringAnswer) {
+        this.reverseStringAnswer = reverseStringAnswer;
+    }
+
+    public String getNormalizationStringAnswer() {
+        return normalizationStringAnswer;
+    }
+
+    public void setNormalizationStringAnswer(String normalizationStringAnswer) {
+        this.normalizationStringAnswer = normalizationStringAnswer;
+    }
+
+    public int getMaxNumericAnswer() {
+        return maxNumericAnswer;
+    }
+
+    public void setMaxNumericAnswer(int maxNumericAnswer) {
+        this.maxNumericAnswer = maxNumericAnswer;
+    }
+
+    public int getUclnNumericAnswer() {
+        return uclnNumericAnswer;
+    }
+
+    public void setUclnNumericAnswer(int uclnNumericAnswer) {
+        this.uclnNumericAnswer = uclnNumericAnswer;
+    }
+
+    public int getBcnnNumericAnswer() {
+        return bcnnNumericAnswer;
+    }
+
+    public void setBcnnNumericAnswer(int bcnnNumericAnswer) {
+        this.bcnnNumericAnswer = bcnnNumericAnswer;
+    }
+
+    public boolean isAlreadyRegistration() {
+        return isAlreadyRegistration;
+    }
+
+    public void setAlreadyRegistration(boolean alreadyRegistration) {
+        isAlreadyRegistration = alreadyRegistration;
+    }
+
+    public boolean isReverseStringAnswerRight() {
+        return isReverseStringAnswerRight;
+    }
+
+    public void setReverseStringAnswerRight(boolean reverseStringAnswerRight) {
+        isReverseStringAnswerRight = reverseStringAnswerRight;
+    }
+
+    public boolean isMaxNumericAnswerRight() {
+        return isMaxNumericAnswerRight;
+    }
+
+    public void setMaxNumericAnswerRight(boolean maxNumericAnswerRight) {
+        isMaxNumericAnswerRight = maxNumericAnswerRight;
+    }
+
+    public boolean isNormalizationStringAnswerRight() {
+        return isNormalizationStringAnswerRight;
+    }
+
+    public void setNormalizationStringAnswerRight(boolean normalizationStringAnswerRight) {
+        isNormalizationStringAnswerRight = normalizationStringAnswerRight;
+    }
+
+    public boolean isBSCNNNumericAnswerRight() {
+        return isBSCNNNumericAnswerRight;
+    }
+
+    public void setBSCNNNumericAnswerRight(boolean BSCNNNumericAnswerRight) {
+        isBSCNNNumericAnswerRight = BSCNNNumericAnswerRight;
+    }
+
+    public boolean isUSCLNNumericAnswerRight() {
+        return isUSCLNNumericAnswerRight;
+    }
+
+    public void setUSCLNNumericAnswerRight(boolean USCLNNumericAnswerRight) {
+        isUSCLNNumericAnswerRight = USCLNNumericAnswerRight;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+            "student=" + student +
+            ", reverseStringAnswer='" + reverseStringAnswer + '\'' +
+            ", normalizationStringAnswer='" + normalizationStringAnswer + '\'' +
+            ", maxNumericAnswer=" + maxNumericAnswer +
+            ", uclnNumericAnswer=" + uclnNumericAnswer +
+            ", bcnnNumericAnswer=" + bcnnNumericAnswer +
+            ", isAlreadyRegistration=" + isAlreadyRegistration +
+            ", isReverseStringAnswerRight=" + isReverseStringAnswerRight +
+            ", isMaxNumericAnswerRight=" + isMaxNumericAnswerRight +
+            ", isNormalizationStringAnswerRight=" + isNormalizationStringAnswerRight +
+            ", isBSCNNNumericAnswerRight=" + isBSCNNNumericAnswerRight +
+            ", isUSCLNNumericAnswerRight=" + isUSCLNNumericAnswerRight +
+            '}';
+    }
 }
--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,16 +1,8 @@
 package model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import java.io.Serializable;
+import java.util.Arrays;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
     private String maSV;
@@ -28,5 +20,111 @@ public class Student implements Serializable {
         this.hovaten = hovaten;
         this.IP = IP;
         this.group = group;
+    }
+
+    public Student() {
+    }
+
+    public Student(String maSV, String hovaten, String IP, int group, String strExamCode1, String strExamCode2, int numericCode3, int numericCode4, int[] numericExam) {
+        this.maSV = maSV;
+        this.hovaten = hovaten;
+        this.IP = IP;
+        this.group = group;
+        this.strExamCode1 = strExamCode1;
+        this.strExamCode2 = strExamCode2;
+        this.numericCode3 = numericCode3;
+        this.numericCode4 = numericCode4;
+        this.numericExam = numericExam;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getHovaten() {
+        return hovaten;
+    }
+
+    public void setHovaten(String hovaten) {
+        this.hovaten = hovaten;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getStrExamCode1() {
+        return strExamCode1;
+    }
+
+    public void setStrExamCode1(String strExamCode1) {
+        this.strExamCode1 = strExamCode1;
+    }
+
+    public String getStrExamCode2() {
+        return strExamCode2;
+    }
+
+    public void setStrExamCode2(String strExamCode2) {
+        this.strExamCode2 = strExamCode2;
+    }
+
+    public int getNumericCode3() {
+        return numericCode3;
+    }
+
+    public void setNumericCode3(int numericCode3) {
+        this.numericCode3 = numericCode3;
+    }
+
+    public int getNumericCode4() {
+        return numericCode4;
+    }
+
+    public void setNumericCode4(int numericCode4) {
+        this.numericCode4 = numericCode4;
+    }
+
+    public int[] getNumericExam() {
+        return numericExam;
+    }
+
+    public void setNumericExam(int[] numericExam) {
+        this.numericExam = numericExam;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "maSV='" + maSV + '\'' +
+            ", hovaten='" + hovaten + '\'' +
+            ", IP='" + IP + '\'' +
+            ", group=" + group +
+            ", strExamCode1='" + strExamCode1 + '\'' +
+            ", strExamCode2='" + strExamCode2 + '\'' +
+            ", numericCode3=" + numericCode3 +
+            ", numericCode4=" + numericCode4 +
+            ", numericExam=" + Arrays.toString(numericExam) +
+            '}';
     }
 }
